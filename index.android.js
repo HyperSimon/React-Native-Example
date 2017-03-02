69f398c926ed3d1d808cac0f3300623088947507
王镇 <hero21c@outlook.com>
2017-03-02 18:26:54 +0800

    新增 图片控件 Image 的使用

--- index.android.js
+++ index.android.js
@@ -6,14 +6,33 @@
 
 
 import React, { Component } from 'react';
-import { AppRegistry, Text } from 'react-native';
+import { AppRegistry, Text, Image, View } from 'react-native';
 
 class AwesomeProject extends Component {
     render() {
         return (
+             <View>
                 <Text>Hello world!</Text>
+                <Bananes></Bananes>
+            </View>
+                
         );
     }
 }
+
+
+
+class Bananes extends Component{
+    render(){
+        let pic = {
+            uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+            
+        };
+
+        return <Image source={pic} style={{width:193, height : 110}}/>
+    };
+}
+
+
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
